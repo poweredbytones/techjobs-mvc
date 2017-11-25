8d785ecc1b850da9c6c4bb0557093d64ff4e8dcb
Anthony Fortner <poweredbytones@gmail.com>
2017-11-25 17:58:01 -0600

    Adding a way to filter results based on search parameters.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -3,7 +3,9 @@ package org.launchcode.controllers;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -13,7 +15,7 @@ import java.util.HashMap;
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping("search")
+@RequestMapping(value = "search")
 public class SearchController {
 
     @RequestMapping(value = "")
@@ -23,5 +25,20 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results" , method = RequestMethod.GET)
+    public String listColumnValues2(Model model) {
+        //model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<String> testData = new ArrayList<>();
+        testData.add("Green");
+        testData.add("red");
+        testData.add("alpha");
+        testData.add("stuff");
+        model.addAttribute("testdata", testData);
+        return "searchResults";
+
+    }
+
+
 
 }
